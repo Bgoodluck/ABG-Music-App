2790cdd04f3ef1ec3911c97f03d2103cd12c46ce
unknown <Bgoodluck>
2024-08-16 14:37:45 +0100

    cloudinary

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -7,7 +7,7 @@ const PlayerContextProvider = (props) => {
     const audioRef = useRef();
     const seekBg = useRef();
     const seekBar = useRef();
-    const url = 'https://abg-music-api.onrender.com';
+    const url = process.env.REACT_APP_API_URL || 'https://abg-music-api.onrender.com';
 
     const [songsData, setSongsData] = useState([]);
     const [albumsData, setAlbumsData] = useState([]);
